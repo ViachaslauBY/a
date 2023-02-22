e86655a923db94ab8468440e104b98e174823e3d
ViachaslauBY <70838254+ViachaslauBY@users.noreply.github.com>
2023-02-22 22:19:32 +0300

    add6

--- js/audio.js
+++ js/audio.js
@@ -1,45 +1,24 @@
-import {showTime} from './time.js';
-import {setBackground} from './slider.js';
-import {getWeather} from './weather.js';
-import {getQuotes} from './quote.js';
-
-showTime();
-setBackground();
-getWeather();
-getQuotes();
-
-
-
-
-
-
-
 import playList from './playList.js';
 
 
 const player = document.querySelector('.player');
-const audio = new Audio()
+const audio = document.querySelector('.audio');
 const playButton = document.querySelector('.play');
 const prevButton = document.querySelector('.play-prev');
 const nextButton = document.querySelector('.play-next');
 const playListContainer = document.querySelector('.play-list');
 
-
 let isPlay = false;
 let playNum = 0;
-let li;
-
 
 const togglePlay = () => {
   if(!isPlay) {
     isPlay = true;
-    // li.classList.add('item-active');    
     playButton.classList.toggle('pause');
     playAudio();
   } else {
     isPlay = false;
-    playButton.classList.toggle('pause');
-    // li.classList.remove('item-active');
+    playButton.classList.toggle('pause')
     pauseAudio();
   }
 }
@@ -54,11 +33,9 @@ const pauseAudio = () => {
   audio.pause();  
 }
 
-playList.forEach(el => {  
-  li = document.createElement('li');  
+playList.forEach((el, i) => {
+  const li = document.createElement('li');
   li.classList.add('play-item');
-  li.textContent = `${el.title}`;
-  playListContainer.append(li);  
+  li.textContent = `${playList[0].title}`;
+  playListContainer.append(li);
 });
-
-playButton.addEventListener('click', togglePlay);
--- js/date.js
+++ js/date.js
@@ -2,6 +2,6 @@ export const showDate = () => {
   const dateBlock = document.querySelector('.date');
   const date = new Date();
   const options = {weekday: 'long', month: 'long', day: 'numeric'};
-  const currentDate = date.toLocaleDateString('en-CA', options);
+  const currentDate = date.toLocaleDateString('en-US', options);
   dateBlock.textContent = currentDate;
 }
